2a3891bffe783dea82f48da8664495cfe9eb6a90
alexhaswarts <154248247+alexhaswarts@users.noreply.github.com>
2023-12-20 10:03:21 -0500

    Add files via upload

---
+++ pianov3.py
@@ -0,0 +1,48 @@
+import tkinter as tk
+import pygame
+
+pygame.init()
+
+def play_sound(note):
+    pygame.mixer.music.load(f"piano_sounds/{note}.wav")
+    pygame.mixer.music.play(0)
+
+def binds(note, key):
+    app.bind(key, lambda event, n=note: play_sound(n))
+
+def binds():
+    keybinds = {
+        "C": "a",
+        "C#": "s",
+        "D": "d",
+        "D#": "f",
+        "E": "g",
+        "F": "h",
+        "F#": "j",
+        "G": "k",
+        "G#": "l",
+        "A": ";",
+        "A#": "'",
+        "B": "Enter"
+    }
+
+    for note, key in keybinds.items():
+        binds(note, key)
+
+app = tk.Tk()
+app.title("Piano")
+
+notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+
+for note in notes:
+    if "#" in note:
+        button = tk.Button(app, text=note, width=10, height=25, bg="black", fg="white", command=lambda n=note: play_sound(n))
+    else:
+        button = tk.Button(app, text=note, width=20, height=30, bg="white", fg="black", command=lambda n=note: play_sound(n))
+    button.grid(row=0, column=notes.index(note))
+
+binds()
+
+pygame.mixer.music.set_volume(1)
+
+app.mainloop()
